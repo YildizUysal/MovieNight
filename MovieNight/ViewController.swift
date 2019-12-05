b4a29f417a787e396dfd944a7c7a10d53a6674b9
Yıldız Uysal <merveyildizuysal@gmail.com>
2019-12-05 15:47:53 +0300

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Yıldız Uysal on 5.12.2019.
+//  Copyright © 2019 YildizUysal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
